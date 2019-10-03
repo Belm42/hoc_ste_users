7fe3d4d7f3a1a293de160ed15965bf62ec553ce2
Lucas Delobelle <lucas@ld-web.net>
2019-10-03 12:12:43 +0200

    Formulaire de contact avec contrôle du tableau POST

--- contact_process.php
+++ contact_process.php
@@ -1,10 +1,22 @@
 <?php
 require_once 'src/Db/functions.php';
 
+// --- Récupération des variables de formulaire -------------------------
+if (empty($_POST['email']) || empty($_POST['message'])) {
+    echo "Les champs fournis ne sont pas valides";
+    die;
+}
+
 $email = $_POST['email'];
 $message = $_POST['message'];
 
-$pdo = getPdoInstance();
+// --- Récupération de l'instance PDO -----------------------------------
+try {
+    $pdo = getPdoInstance();
+} catch (PDOException $e) {
+    echo 'Erreur lors de la connexion à la base de données';
+    die;
+}
 
 $stmt = $pdo->prepare("INSERT INTO contact (email, message) VALUES (:email, :mess)");
 
